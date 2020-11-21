1bd0fba486a8d6f5da2d07590bbe2bcb19e2d2d9
jonkr2 <74167264+jonkr2@users.noreply.github.com>
2020-11-21 09:05:57 -0800

    Add files via upload

--- NN/trainNet.py
+++ NN/trainNet.py
@@ -29,37 +29,44 @@ def saveWeightsText( fileName ):
 for argNum in range(1, len(sys.argv) ):
 
         netName = sys.argv[argNum];
-        dataFilename = netName + "Data.dat";
+        dataFilename = netName + "Data.dat"
         labelFilename = netName + "Labels.dat"
+        structureFilename = netName + "Struct.txt"
 
-        # TODO : structure file
-        numInputs = (32 + 28) * 2 + 1
-        epochsParam = 30 # good values for this do depend somewhat on number of training positions
+        # good values for these depend somewhat on number of training positions
+        epochsParam = 30 
         batchSizeParam = 15000
-        # layers, neurons, epochs?
- 
+
+        # Load Net Structure
+        netStructure = np.loadtxt( structureFilename );
+        numInputs = int(netStructure[0])
+        numLayers = int(netStructure[1])
+        layerSizes = netStructure[2:6]
+        print("numInputs : " + str(numInputs) + " layers : " + str(numLayers) )
+        print(layerSizes)
+
         # Load input data for each position
         print("Loading Positions : " + dataFilename )
         f = open(dataFilename, 'rb')
-        chessData = np.fromfile(f, dtype=np.int8) 
-        chessData = chessData.reshape(-1, numInputs)
+        positionData = np.fromfile(f, dtype=np.int8) 
+        positionData = positionData.reshape(-1, numInputs)
         f.close()
 
         # Load target labels for each position
         print("Loading Labels : " + labelFilename)
         f = open(labelFilename, 'rb')
-        chessLabels = np.fromfile(f, dtype=np.float32) 
+        positionLabels = np.fromfile(f, dtype=np.float32) 
         f.close()
 
-        print ("positionLen:", len(chessData))
-        print ("labelLen:", len(chessLabels))
+        print ("dataLen:", format(len(positionData), ",") )
+        print ("labelLen:", format(len(positionLabels), ",") )
 
         # Create the neural net model
         model = keras.Sequential([	
-                keras.layers.Dense(192, activation="relu"),
-                keras.layers.Dense(32, activation="relu"),
-                keras.layers.Dense(32, activation="relu"),
-                keras.layers.Dense(1, activation="sigmoid"), # use sigmoid for our 0-1 training labels
+                keras.layers.Dense(layerSizes[0], activation="relu"),
+                keras.layers.Dense(layerSizes[1], activation="relu"),
+                keras.layers.Dense(layerSizes[2], activation="relu"),
+                keras.layers.Dense(layerSizes[3], activation="sigmoid"), # use sigmoid for our 0-1 training labels
                 ])
 
         lr_schedule = keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=.01,decay_steps=5000,decay_rate=0.96)
@@ -68,12 +75,12 @@ for argNum in range(1, len(sys.argv) ):
         #not sure what loss function should be or if should use sigmoid activation
         model.compile(optimizer=opt, loss="mean_squared_error")
 
-        model.fit(chessData, chessLabels, batch_size= batchSizeParam, epochs= epochsParam )
+        model.fit(positionData, positionLabels, batch_size= batchSizeParam, epochs= epochsParam )
 
         # print some predictions for a quick sanity check
-        prediction = model.predict( chessData[0:50] )
+        prediction = model.predict( positionData[0:50] )
         for x in range(0, 50):
-                print( str(x+1) + " prediction = " + str(prediction[x]) + "  target = " + str(chessLabels[x]) )
+                print( str(x+1) + " prediction = " + str(prediction[x]) + "  target = " + str(positionLabels[x]) )
 
         # Save the trained weights
         weightFilename =  netName + "Weights.txt";
